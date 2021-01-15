6c0c1643c3fe4a17911a422bd4f98eb164148ce5
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-01-15 17:09:44 +0100

    playing with navbar, react social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function NavBar() {
   return (
     <div>
-      <header>
-        <div>
-          <NavLink to='/' exact>
-            Home
-          </NavLink>
-          <NavLink to='/projects'>
-            Projects
-          </NavLink>
-          <NavLink to='/about'>
-            About me
-          </NavLink>
+      {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+          
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/projects">Projetcs</a>
+            </li>
+          </ul>
         </div>
-      </header>
+      </nav> */}
+
+
+      <nav className='navbar navbar-expand-sm navbar-light bg-purple'>
+        {/* <div> */}
+          <button className="navbar-toggler color-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon color-white"></span>
+          </button>
+          <div className="container">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div className="d-flex justify-content-start">
+              <NavLink to='/' exact className="nav-link" activeClassName="active-nav-link">
+                Home
+              </NavLink>
+              <NavLink to='/projects' className="nav-link" activeClassName="active-nav-link">
+                Projects
+              </NavLink>
+              <NavLink to='/about' className="nav-link" activeClassName="active-nav-link">
+                About me
+              </NavLink>
+            </div>
+          </div>
+        {/* </div> */}
+
+        <div className="social-icons">
+          <SocialIcon url="https://www.linkedin.com/in/dorota-szot-7848a0169/" target="blank" fgColor="#fff" style={{ height: 35, width: 35 }} bgColor="transparent" className="mr-3"/>
+          <SocialIcon url="https://github.com/dorotaszot" target="blank" fgColor="#fff" style={{ height: 35, width: 35 }} bgColor="transparent"/>
+        </div>
+        </div>
+      </nav>
     </div>
   )
 }
